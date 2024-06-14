ba733f22130cab13c505eb3d3731045f8efe0593
aks12335 <akumar@esri.com>
2024-06-13 22:11:37 -0700

    initial site

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {},
+    fontFamily: {
+      "comic-sans": ["Comic Sans MS", "Comic Sans"],
+      custom: ["Covered By Your Grace", "cursive"],
+    },
+  },
+  plugins: [],
+};
