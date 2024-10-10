8f8fc235c21ae4cdbfcd5d63c8921db5ace6b57c
aks12335 <akumar@esri.com>
2024-10-09 22:35:17 -0700

    added password

--- src/components/home.js
+++ src/components/home.js
@@ -24,59 +24,114 @@ const Home = () => {
     //     </a>
     //   </div>
     // </header>
-    <div class="bg">
-      <header class="header">
-        <div class="flex container mx-20">
-          <img
-            class="profile-image img-responsive flex justify-start"
-            src={myImg}
-            alt="Akshay Kumar"
-            width="250"
-            height="250"
-          />
-          <div class="profile-content flex flex-col items-start">
-            <h1 class="name">Akshay Kumar</h1>
-            <h2 class="desc">
-              Software Engineer | Technology Enthusiast | Basketball fan
-            </h2>
-            <ul class="social flex flex-row">
-              <li>
-                <a href="https://www.linkedin.com/in/welcome-to-akshays-profile/">
-                  <i class="fa fa-linkedin"></i>
-                </a>
-              </li>
-              <li class="ml-4">
-                <a href="https://github.com/imdoom">
-                  <i class="fa fa-github-alt"></i>
-                </a>
-              </li>
-            </ul>
+    <div>
+      <div class="bg2">
+        <header class="header">
+          <div class="flex container mx-20">
+            <img
+              class="profile-image img-responsive flex justify-start"
+              src={myImg}
+              alt="Akshay Kumar"
+              width="20%"
+              height="20%"
+            />
+            <div class="profile-content flex flex-row">
+              <div class="flex flex-col items-start">
+                <h1 class="name font-custom">Akshay Kumar</h1>
+                <h2 class="desc font-custom">
+                  Software Engineer | Technology Enthusiast | Basketball fan
+                </h2>
+                <ul class="social flex flex-row">
+                  <li>
+                    <a href="https://www.linkedin.com/in/welcome-to-akshays-profile/">
+                      <i class="fa fa-linkedin"></i>
+                    </a>
+                  </li>
+                  <li class="ml-4">
+                    <a href="https://github.com/imdoom">
+                      <i class="fa fa-github-alt"></i>
+                    </a>
+                  </li>
+                  <li>
+                    <div class="projects ml-4 font-custom">Projects</div>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </header>
+        <div>
+          <section class="about-me">
+            <div class="about-me-heading font-custom  mt-8">About Me</div>
+            <div>
+              <p class="font-custom">
+                Hi! I started my front-end journey 6 years ago when I was asked
+                to build a progress dashboard for my team at my first job. Ever
+                since I have been dabbling in banners, navbars, buttons,
+                dropdowns and the like. I am a visual person so the allure of
+                what you code is what you see was compeling enough for me to
+                keep tinkering with UI. I moved to the US about 5 years ago
+                where I pursued my masters degree in Computer Science at
+                Northwestern University. Over there, I focused on human computer
+                interaction and took a bunch of UI focused projects and courses.
+                I learnt a lot about usability, rapid prototyping, design,
+                working in sprint teams, React/JSX, etc. My career includes
+                stints with two multinational corporations. I'm looking for my
+                next venture where I can express myself creatively and work on
+                interesting collaborative projects.
+              </p>
+            </div>
+          </section>
+        </div>
+      </div>
+      <div class="bg">
+        <div class="flex games-container">
+          <div>
+            <a
+              class="block mx-10 max-w-sm p-6 border border-black bg-white rounded-lg shadow
+           hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700"
+            >
+              <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+                <button className="bg-yellow-100 px-4 text-gray-800 ml-4 rounded-full">
+                  More games incoming...
+                </button>
+              </h5>
+              <p class="font-normal text-gray-700 dark:text-gray-400">
+                More games incoming...
+              </p>
+            </a>
+          </div>
+          <Link to="/password">
+            <a
+              class="block max-w-sm p-6 border border-black bg-white rounded-lg shadow
+           hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700"
+            >
+              <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+                <button className="bg-blue-500 px-4 text-white ml-4 rounded-full">
+                  The password game
+                </button>
+              </h5>
+              <p class="font-normal text-gray-700 dark:text-gray-400">
+                &^$#(@*#()
+              </p>
+            </a>
+          </Link>
+          <div>
+            <a
+              class="block mx-10 max-w-sm p-6 border border-black bg-white rounded-lg shadow
+           hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700"
+            >
+              <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+                <button className="bg-yellow-100 px-4 text-gray-800 ml-4 rounded-full">
+                  More games incoming...
+                </button>
+              </h5>
+              <p class="font-normal text-gray-700 dark:text-gray-400">
+                More games incoming...
+              </p>
+            </a>
           </div>
         </div>
-      </header>
-      <div class="container">
-        <section>
-          <h2 class="heading about-me-heading font-custom">About Me</h2>
-          <div class="about-me mt-3">
-            <p class="font-custom">
-              Hi! I started my front-end journey 6 years ago when I was asked to
-              build a progress monitor dashboard for my team at my first job.
-              Ever since I have been dabbling in banners, navbars, buttons,
-              dropdowns and the like. I am a visual person so the allure of what
-              you code is what you see was compeling enough for me to keep
-              tinkering with UI. I moved to the US about 5 years ago where I
-              pursued my masters degree in Computer Science from Northwestern
-              University. Over there, I look a lot of UI/UX courses and did a
-              bunch of group front-end projects. I learnt a lot of about coming
-              up with new ideas quickly, prototyping, designing and quickly
-              deploying websites. I have worked at two MNCs with customers
-              across regions with sprint styled teams and delivered projects
-              under under tight deadlines. I'm looking for my next venture where
-              I can express myself creatively and work on complex and
-              interesting projects.
-            </p>
-          </div>
-        </section>
       </div>
     </div>
   );
--- src/components/password.js
+++ src/components/password.js
@@ -1,18 +1,125 @@
-import React from "react";
+import { React, useState, useEffect } from "react";
+
+const rules = {
+  ".{6,}": "The password should be minimum 6 characters",
+  "^(?=.*[0-9]).{6,}$": "The password should have atleast one number.",
+  "^(?=.*[0-9])(?=.*[A-Z]).{6,}$":
+    "The password should have atleast a capital letter",
+  "^(?=.*[0-9])(?=.*[A-Z])(?=.*[!@#$%^&*)(]).{6,}$":
+    "The password should have atleast one special character",
+  "(?=.*[A-Z0-9a-z])": "The password digits should add upto 13",
+};
+
 const Password = () => {
+  const [password, setPassword] = useState("");
+  const rulesArr = Object.keys(rules);
+  const [currRule, setCurrRule] = useState(0);
+
+  useEffect(() => {
+    if (!password) {
+      setProcessed([]);
+      setCurrRule(0);
+    } else {
+    }
+
+    if (currRule < rulesArr.length) {
+      rulesArr.find((rule) => {
+        if (rule in processed) {
+          return false;
+        } else {
+          const regex = new RegExp(rule);
+          if (rules[rule] === "The password digits should add upto 13") {
+            let chars = [...password],
+              sum = 0;
+            chars.forEach((char) => {
+              console.log(Number(char));
+              if (Number(char)) {
+                sum += Number(char);
+              }
+            });
+            if (sum === 13) {
+              return true;
+            }
+          } else {
+            if (regex.test(password)) {
+              return true;
+            }
+          }
+          return false;
+        }
+      });
+    }
+  }, [password]);
+
+  function getAlerts() {
+    return (
+      <div>
+        {currRule ? null : (
+          <div
+            className="flex items-center m-auto p-4 mb-4 w-3/6 text-sm text-red-800 border border-red-300 rounded-lg bg-red-50 dark:bg-gray-800 dark:text-red-400 dark:border-red-800"
+            role="alert"
+            key={rules[currRule]}
+          >
+            <svg
+              className="flex-shrink-0 inline w-4 h-4 me-3"
+              aria-hidden="true"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="currentColor"
+              viewBox="0 0 20 20"
+            >
+              <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5ZM9.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM12 15H8a1 1 0 0 1 0-2h1v-3H8a1 1 0 0 1 0-2h2a1 1 0 0 1 1 1v4h1a1 1 0 0 1 0 2Z" />
+            </svg>
+            <span className="sr-only">Info</span>
+            <div>
+              <span className="font-medium">{rules[rule]}</span>
+            </div>
+          </div>
+        )}
+        {processed.map((rule) => (
+          <div
+            class="flex items-center m-auto p-4 mb-4 w-3/6 text-sm text-green-800 rounded-lg bg-green-50 dark:bg-gray-800 dark:text-green-400"
+            role="alert"
+            key={rules[rule]}
+          >
+            <svg
+              class="flex-shrink-0 inline w-4 h-4 me-3"
+              aria-hidden="true"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="currentColor"
+              viewBox="0 0 20 20"
+            >
+              <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5ZM9.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM12 15H8a1 1 0 0 1 0-2h1v-3H8a1 1 0 0 1 0-2h2a1 1 0 0 1 1 1v4h1a1 1 0 0 1 0 2Z" />
+            </svg>
+            <span class="sr-only">Info</span>
+            <div>
+              <span className="font-medium">{rules[rule]}</span>
+            </div>
+          </div>
+        ))}
+      </div>
+    );
+  }
+
   return (
     <>
-      <p class="text-4xl pt-6">* The password game</p>
-      <div class="mb-6 mt-10">
+      <p className="text-4xl pt-6">* The password game</p>
+      <div className="mb-6 mt-10">
         <label
           for="large-input"
-          class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+          className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
         >
           Please choose a password
         </label>
-        <input type="text" id="large-input" />
+        <input
+          type="text"
+          id="large-input"
+          onChange={(ev) => setPassword(ev.target.value)}
+          className="border-solid border-2 border-indigo-600"
+        />
       </div>
+      {getAlerts()}
     </>
   );
 };
+
 export default Password;
